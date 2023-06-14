d27672dd7d9355a94ed17d35a914a32ec620bb27
kss <tjdtn6534@naver.com>
2023-06-14 19:40:38 +0900

    20230614

--- daily-commit/20230613.cpp
+++ daily-commit/20230613.cpp
@@ -1,35 +1,35 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include<iostream>
-
-using namespace std;
-
-string solution(string input_string) {
-    string answer = "";
-    bool check = false;
-    for (int i = 0; i < input_string.length() - 1; i++) {
-        char out = input_string[i];
-        if (answer.find(out) != string::npos)continue;
-        for (int j = i + 1; j < input_string.length(); j++) {
-            if (out != input_string[j] && !check) {
-                check = true;
-                continue;
-            }
-            if (out == input_string[j] && check) {
-                answer += out;
-                check = false;
-                break;
-            }
-            if (j == input_string.length() - 1)check = false;
-        }
-    }
-    sort(answer.begin(), answer.end());
-    if (answer == "")return "N";
-    return answer;
-}
-
-int main() {
-    cout << solution("aaabaaa") << endl;
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//#include<iostream>
+//
+//using namespace std;
+//
+//string solution(string input_string) {
+//    string answer = "";
+//    bool check = false;
+//    for (int i = 0; i < input_string.length() - 1; i++) {
+//        char out = input_string[i];
+//        if (answer.find(out) != string::npos)continue;
+//        for (int j = i + 1; j < input_string.length(); j++) {
+//            if (out != input_string[j] && !check) {
+//                check = true;
+//                continue;
+//            }
+//            if (out == input_string[j] && check) {
+//                answer += out;
+//                check = false;
+//                break;
+//            }
+//            if (j == input_string.length() - 1)check = false;
+//        }
+//    }
+//    sort(answer.begin(), answer.end());
+//    if (answer == "")return "n";
+//    return answer;
+//}
+//
+//int main() {
+//    cout << solution("aaabaaa") << endl;
+//    return 0;
+//}
---
+++ daily-commit/20230614.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> t_idx;
+int Max = 0;
+
+void dfs(vector<vector<int>> ability, int y, int result) {
+    if (t_idx.size() == ability[0].size()) {
+        if (Max < result) {
+            Max = result;
+        }
+        return;
+    }
+    for (int i = 0; i < ability.size(); i++) {
+        if (find(t_idx.begin(), t_idx.end(), i) == t_idx.end()) {
+            t_idx.push_back(i);
+            dfs(ability, y + 1, result + ability[i][y]);
+            t_idx.erase(t_idx.end() - 1);
+        }
+    }
+}
+
+int solution(vector<vector<int>> ability) {
+    int answer = 0;
+
+    dfs(ability, 0, 0);
+
+    return Max;
+}
+
+int main() {
+    cout << solution({ {40, 10, 10} ,{20, 5, 0},{30, 30, 30},{70, 0, 70},{100, 100, 100} })<< endl;
+
+    return 0;
+}
