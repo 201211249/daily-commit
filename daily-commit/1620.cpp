6cccbc7217c5a03df393c52398a016fbdaa3ac7d
kss <tjdtn6534@naver.com>
2023-06-30 20:04:35 +0900

    20230630

--- daily-commit/1620.cpp
+++ daily-commit/1620.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, m;
+string s, a[100004];
+map<string, int> mp;
 int main() {
-	cout << "test" << endl;
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 1; i <=n; i++) {
+		cin >> s;
+		a[i] = s;
+		mp[s] = i ;
+	}
+	
+	for (int i = 0; i < m; i++) {
+		cin >> s;
+		if (atoi(s.c_str()) == 0) cout << mp[s] << "\n";
+		else cout << a[atoi(s.c_str())] << "\n";
+	}
 	return 0;
 }
